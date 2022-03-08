9f1007c93b38efbdcb2d2764010ef1c09e13bf99
Hu1buerger <hu1buerger@gmail.com>
2022-03-08 23:12:38 +0100

    changed the arbitrary instance and ==> for generation UnificationTest.prop_test3

--- test/UnificationTest.hs
+++ test/UnificationTest.hs
@@ -13,15 +13,21 @@ prop_test1 t = ds t t == Nothing
 prop_test2 :: Term -> Term -> Property
 prop_test2 t t1 = ds t t1 /= Nothing ==> t /= t1
 
-prop_test3a :: Term -> Term -> Property
-prop_test3a t1 t2 = ds t1 t2 == Nothing ==> unify t1 t2 /= Nothing
-prop_test3b :: Term -> Term -> Property
-prop_test3b t1 t2 = ds t1 t2 == Nothing ==> null (domain . fromJust $ unify t1 t2)
+prop_test3 :: NothingDS -> Bool
+prop_test3 (NothingDS t1 t2) = unify t1 t2 /= Nothing && null (domain . fromJust $ unify t1 t2)
 
 prop_test4 :: Term -> Term -> Property
 prop_test4 t1 t2 = unify t1 t2 /= Nothing ==> (ds (apply unt1t2 t1) (apply unt1t2 t2)) == Nothing
     where unt1t2 = (fromJust $ unify t1 t2)
-    
+
+data NothingDS = NothingDS Term Term deriving (Show)
+data JustDS = JustDS Term Term deriving (Show)
+
+instance Arbitrary NothingDS where
+    arbitrary = (uncurry NothingDS <$> (suchThat arbitrary (\(a,b) -> ds a b == Nothing)))
+instance Arbitrary JustDS where 
+    arbitrary = (uncurry JustDS <$> (suchThat arbitrary (\(a,b) -> ds a b /= Nothing)))
+
 -- Check all properties in this module:
 return []
 testAll = $quickCheckAll
